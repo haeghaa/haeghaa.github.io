1fab6dc3f2a1eda0c61c45df1344f85aca0aa741
haeghaa <haeghaa@gmail..com>
2025-03-21 16:48:50 +0900

    changes the jekyll.yml

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742542545',resources: [
+    cacheName: 'chirpy-1742543325',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
